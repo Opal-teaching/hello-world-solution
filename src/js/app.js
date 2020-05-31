e3c86b0b5010ac24c1ad12501b7d3537ec8a1e42
Stacey Beard <staceybeard10@gmail.com>
2020-05-31 10:06:06 -0400

    Update comments

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,4 @@ import "onsenui/css/onsen-css-components-blue-basic-theme.css";
 import "onsenui/css/onsenui.css";
 
 const app = angular
-    .module('HelloWorld', ['onsen']);
+    .module('HelloWorld', ['onsen']); // Defines the HelloWorld module and its dependencies
--- src/js/controllers/helloWorldController.js
+++ src/js/controllers/helloWorldController.js
@@ -1,15 +1,15 @@
-(function(){
+(function(){ // Wraps the controller in an Immediately Invoked Function Expression (as per the Johnpapa style guide) to limit variable scope
 	'use strict';
 	
 	angular
-		.module('HelloWorld') // Fetches the angular the module so that the controller can access it.
-		.controller("HelloWorldController", HelloWorldController); // Declares the component in the angular module
+		.module('HelloWorld') // Fetches the angular module so that the controller can access it
+		.controller("HelloWorldController", HelloWorldController); // Declares the controller component in the angular module and connects it to the function below
 
 	HelloWorldController.$inject = []; // Injects the angular dependencies for this controller (there are none)
 
 	/* @ngInject */
-	function HelloWorldController() { // function representing this controller. vm is the equivalent of $scope
-		const vm = this;
-		vm.hello = "Hello World!";
+	function HelloWorldController() { // Function representing this controller
+		const vm = this; // vm is the equivalent of $scope
+		vm.hello = "Hello World!"; // Declare a controller variable than can be used in its view
 	}
 })();
