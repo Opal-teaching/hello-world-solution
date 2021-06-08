c52fb3dec5955bb469217d930c34f33395fb3706
Stacey Beard <staceybeard10@gmail.com>
2021-06-08 12:40:06 -0400

    Set up a test to check onsenui v2's latest release (without webpack)

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import angular from "angular";
-import "onsenui/css/onsenui.css";
-import "onsenui/css/onsen-css-components.css";
-
-window.ons = require('onsenui/js/onsenui');
-require('onsenui/js/angular-onsenui');
-
-angular.module('HelloWorld', ['onsen']); // Defines the HelloWorld module and its dependencies
